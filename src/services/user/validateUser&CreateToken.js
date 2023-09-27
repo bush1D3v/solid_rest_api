145298130b42ecbd60cbc5e2bc49bf3851a9a21d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-09-27 00:42:32 -0300

    feat(validateUser&CreateToken.js) exporta servico que valida usuario e cria o seu token

---
+++ src/services/user/validateUser&CreateToken.js
@@ -0,0 +1,24 @@
+const {
+  validateUser,
+  createToken
+} = require('../../repositories/user/index')
+
+const validateUserAndCreateToken = async (name, mail, password) => {
+  try {
+    const user = await validateUser(name, mail, password)
+
+    if (typeof user == 'string') {
+      return 'Name, Email or Password invalid'
+    }
+
+    const token = createToken(user)
+
+    return token
+  } catch (error) {
+    return error.message
+  }
+}
+
+module.exports = {
+  validateUserAndCreateToken
+}

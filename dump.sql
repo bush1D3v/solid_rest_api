0093397d257548c48141918531ad69b0abe785c7
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-09-26 23:44:32 -0300

    update(dump.sql) criacao de tabelas sql

---
+++ dump.sql
@@ -0,0 +1,15 @@
+create table usuario (
+  id serial primary key,
+  nome text not null,
+  email text not null unique,
+  senha text not null
+);
+
+create table pokemons (
+  id serial primary key,
+  usuario_id integer references usuarios(id) not null,
+  nome text not null,
+  habilidades text not null,
+  imagem text,
+  apelido text
+);

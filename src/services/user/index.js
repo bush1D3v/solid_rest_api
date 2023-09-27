af096f4acd0497868faae6b32483529870664284
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-09-26 23:51:23 -0300

    feat(index.js) exporta todos os services de 'user'

---
+++ src/services/user/index.js
@@ -0,0 +1,7 @@
+const { createAndReturnUser } = require('./create&ReturnUser')
+const { validateUserAndCreateToken } = require('./validateUser&CreateToken')
+
+module.exports = {
+  createAndReturnUser,
+  validateUserAndCreateToken
+}

3e6542348b916e474158154de3d70f8a9f4935bf
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-09-27 00:24:56 -0300

    feat(createPokemon.js) exporta repositorio que cria pokemon

---
+++ src/repositories/pokemon/createPokemon.js
@@ -0,0 +1,23 @@
+const pool = require('../../data/conection')
+
+const createPokemon = async (id, nome, apelido, habilidades, imagem) => {
+  try {
+    const params = [id, nome, habilidades, imagem, apelido]
+
+    const { rows } = await pool.query(`
+      insert into pokemons
+      (usuario_id, nome, habilidades, imagem, apelido)
+      values
+      ($1, $2, $3, $4, $5)
+      returning *
+    `, params)
+
+    return rows
+  } catch (error) {
+    return error.message
+  }
+}
+
+module.exports = {
+  createPokemon
+}

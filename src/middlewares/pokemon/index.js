f96af11ffcee37d77ebf866d27ea248fba397a17
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-09-26 23:50:28 -0300

    feat(index.js) exporta todos os middlewares de 'pokemon'

---
+++ src/middlewares/pokemon/index.js
@@ -0,0 +1,9 @@
+const { verifyIdParam } = require('./verifyIdParam')
+const { verifyNameHabilitiesBody } = require('./verifyNameHabilitiesBody')
+const { verifyNicknameBody } = require('./verifyNicknameBody')
+
+module.exports = {
+  verifyIdParam,
+  verifyNameHabilitiesBody,
+  verifyNicknameBody
+}

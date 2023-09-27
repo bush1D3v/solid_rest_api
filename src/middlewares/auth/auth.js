6ef175c3eff8e27be5b81faf48ecf51a30a6e358
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-09-27 00:16:26 -0300

    feat(auth.js) exporta middleware que autentica o usuario

---
+++ src/middlewares/auth/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const jwtpassword = require('../../password/jwtPassword')
+
+const auth = (req, resp, next) => {
+  try {
+    const { authorization } = req.headers
+
+    if (!authorization) {
+      return resp.status(401).json({message: "Unauthorized"})
+    }
+
+    const token = authorization.split(' ')[1]
+
+    const userToken = jwt.verify(token, jwtpassword)
+
+    next()
+  } catch (error) {
+    resp.status(401).json({message: error.message})
+  }
+}
+
+module.exports = {
+  auth
+}

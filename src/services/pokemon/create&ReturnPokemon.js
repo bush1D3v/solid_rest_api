ee5cdb409c63b2a9569b80fdfc2fb4f637b97a27
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-09-27 00:35:31 -0300

    feat(create&ReturnPokemon.js) exporta servico que cria pokemon no bd e retorna-o como resposta da requisicao

---
+++ src/services/pokemon/create&ReturnPokemon.js
@@ -0,0 +1,19 @@
+const {
+  getUserId,
+  createPokemon
+} = require('../../repositories/pokemon/index')
+
+const createAndReturnPokemon = async (req, nome, apelido, habilidades, imagem) => {
+  try {
+    const id = getUserId(req)
+    const pokemon = await createPokemon(id, nome, apelido, habilidades, imagem)
+
+    return pokemon
+  } catch (error) {
+    return console.log(`services/pokemon/createAndReturnPokemon: ${error.message}`);
+  }
+}
+
+module.exports = {
+  createAndReturnPokemon
+}
